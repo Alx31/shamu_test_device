8b8c9639b1ecb6c552927edca9e3b7d076c987f3
Mikhail Naganov <mnaganov@google.com>
2016-11-21 15:22:17 -0800

    Add all audio HIDL *-impl libraries to the list of product packages
    As *-impl.so files are loaded via dlopen, they don't get added
    automatically with the audio HAL server.
    
    Test: check files on the device
    Change-Id: I9ec8e0b7883d88f3077fcb425a33f19162b2f1df

--- device.mk
+++ device.mk
@@ -179,7 +179,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
