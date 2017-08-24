7ac37532f2e5ee590d8507c7c77dd8daf75d78e0
theringmaster <gzroms@hotmail.com>
2017-08-24 16:27:44 -0400

    Add Gzosp.mk

---
+++ gzosp.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+#$(call inherit-product, vendor/gzosp/config/caf_fw.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-gzr
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := gzosp_shamu
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6
+
+TARGET_VENDOR := moto
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    DEVICE_MAINTAINERS="John Brewer(TheRingMaster)" \
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N8I11B 4171878 release-keys"
