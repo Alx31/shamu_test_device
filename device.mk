LineageOS/android_device_huawei_angler

Added light hal.
Bug: 32022100
Test: pass
Change-Id: Ib4b37a86fc278d7ff94d65885ea999f4fd409085

device.mk
@@ -183,8 +183,12 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8994 \
     libgenlock \
     memtrack.msm8994 \
-    lights.angler \
     android.hardware.memtrack@1.0-impl
+
+# Light HAL
+PRODUCT_PACKAGES += \
+    lights.angler \
+    android.hardware.light@2.0-impl
 
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_PACKAGES += \

Alx31/shamu_test_device

device.mk
@@ -233,7 +233,8 @@ PRODUCT_PACKAGES += \
     libion
 
 PRODUCT_PACKAGES += \
-    lights.shamu
+    lights.shamu \
+    android.hardware.light@2.0-impl
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \